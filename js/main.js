ab6fb586988420938ffa31a8225bc88c61347171
luiginspiron <luiginspiron@gmail.com>
2020-04-22 21:42:06 -0300

    cfc 1ra version

---
+++ js/main.js
@@ -0,0 +1,56 @@
+(function(){
+    "use strict";
+
+    var regalo = document.getElementById('regalo');
+
+    document.addEventListener('DOMContentLoaded', function(){
+
+        if(document.getElementById('mapa')){
+            var map = L.map('mapa').setView([-27.347483, -65.60002], 17);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker([-27.347483, -65.60002]).addTo(map)
+            .bindPopup('Stewart Shipton 1980, Concepción(4146) - TUC')
+            .openPopup()
+            .bindTooltip('Concepción Fútbol Club')
+            .openTooltip();
+        }
+    }); //DOM CONTENT LOADED
+})();
+
+$(function() {
+
+    // Menu fijo
+    var windowHeight = $(window).height();
+    var barraAltura = $('.barra').innerHeight();
+
+    console.log (barraAltura);
+    
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > windowHeight){
+            $('.barra').addClass('fixed');
+            $('body').css({'margin-top': barraAltura+'px'});
+        } else{
+            $('.barra').removeClass('fixed');
+            $('body').css({'margin-top': '0px'});
+        }
+    });
+
+    //Menu responsive
+    $('.menu-movil').on('click', function(){
+        $('.navegacion-principal').slideToggle();
+    });
+
+    $(window).resize(function() {
+        var windowWidth = $(".barra").width();
+        if (windowWidth > 755) {
+          $(".navegacion-principal").css({ display: "block" });
+        } else {
+          $(".navegacion-principal").css({ display: "none" });
+        }
+    });
+});
